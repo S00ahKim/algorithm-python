2eafb49f47dfb327491d8f3fc51fb1f4766f7b6d
rlatndk0206 <rlatndk0206@naver.com>
2019-08-29 13:38:11 +0900

    기윤이네 텃밭(수정)

--- kiyoon_garden.py
+++ kiyoon_garden.py
@@ -19,59 +19,76 @@
 구역의 개수를, 셋째 줄에 토마토만 심어져 있는 구역의 개수를 출력한다
 
 '''
-# 밭이 하나의 작물로 이루어져 있는지 check
-# arr: 2차원 배열 / x, y: 검증할 밭의 시작점 / len: 검증할 밭의 길이
-def check(arr, x, y, len):
-    global ltc, tmt, ccb
-    base = arr[x][y]
-    checked = True
-    for i in range(len):
-        for j in range(len):
-            if base != arr[x+i][y+j]:
-                checked == False
-                divide(arr, x, y, len)
-                return
-
-    # 각 채소들의 밭의 갯수를 하나씩 더해준다
-    if checked == True:
-        if base == -1:
-            ltc += 1
-        elif base == 0:
-            ccb += 1
-        else :
-            tmt += 1
-
-# 밭을 9개로 나누는 함수 divide
-# arr: 2차원 배열 / x, y: 나눌 밭의 시작점 / len: 나눠야 할 밭의 길이
-def divide(arr, x, y, len):
-    mul = int(len/3) # 나눠진 밭의 길이
-    xrr = [] # 나눠진 밭들의 시작점 x 좌표
-    yrr = [] # 나눠진 밭들의 시작점 y 좌표
-    for i in range(3):
-        xrr.append(x+i*mul)
-        xrr.append(x+i*mul)
-        xrr.append(x+i*mul)
-    for _ in range(3):
-        yrr.append(x+0*mul)
-        yrr.append(x+1*mul)
-        yrr.append(x+2*mul)
+def wk5_4():
+    NN = int(input())
+    land = [[int(elm) for elm in input().split(' ')] for _ in range(NN)] # 한 줄씩 받은 입력값을 NxN 행렬로 만든다.
+    check(land)
+ 
+    for count in count_list:
+        print(count)
+ 
+def check(land):
+    '''
+    입력으로 받은 구역에
+        같은 작물이 있는 경우, 각 작물이 심어진 구역의 개수 출력
+        다른 작물이 있는 경우, 9개의 sub 구역으로 분리
+    '''
+    if is_same(land): # 같은 작물이 심어졌을 때가 종료조건
+        count(land)
+    else:            
+        for each_3 in split_into_9_lists(land): # 우선 3개의 큰 구역들로 구분된다.
+            for each in each_3: # 구분된 큰 구역은 각각 3개의 구역을 포함하고 있다.
+                check(each) # 구분된 9개의 각 구역에 재귀 호출
+ 
+def count(land):
+    '''
+    이 함수의 input으로 주어지는 구역은 모두 같은 원소로 이루어졌습니다.
+    따라서 원소의 종류(상추, 오이, 토마토)를 구분하여 전역변수에 그 개수만큼 추가합니다.
+    '''
+    SANGCHU, OI, TOMATO = -1, 0, 1
+    first_elm = land[0][0] # 모두 같은 작물이 심어져 있으니, 첫 번째 원소로 판단
     
-    for i in range(9):
-        check(arr, xrr[i], yrr[i], mul)
-
-
-
-c_len = int(input()) # 밭의 한변의 길이 값
-arr = [] # 밭의 채소 값들이 입력될 배열
-ltc = 0 # 상추
-ccb = 0 # 오이
-tmt = 0 # 토마토
-
-for _ in range(c_len):
-    line = input()
-    arr.append(list(map(int, line.split(' '))))
-
-check(arr, 0, 0, c_len)
-print(ltc)
-print(ccb)
-print(tmt)
+    if first_elm == SANGCHU:
+        count_list[0] += 1
+    elif first_elm == OI:
+        count_list[1] += 1
+    elif first_elm == TOMATO:
+        count_list[2] += 1
+    else:
+        print('오류')
+ 
+def split_into_9_lists(land): # list를 9개의 sub-list로 분할하여 저장
+    '''
+    land는 NxN list입니다.
+    주어진 구역을 9등분해야하기 때문에 전체 길이의 1/3만큼을 나뉠 구역의 길이로 설정합니다.
+    우선 land를 가로로 3등분하였고, 그 후 세로로 3등분하여 별도의 list에 저장하였습니다.
+    total = 9, small = 3을 예시로 대입해보시면 이해가 잘 될 것 같습니다.
+    결과값은 다음과 같은 형태일 것입니다.
+    [
+        [ [1구역], [2구역], [3구역] ],
+        [ [4구역], [5구역], [6구역] ],
+        [ [7구역], [8구역], [9구역] ],
+    ]
+    '''
+    total = len(land)   # 한 구역의 가로 길이
+    small = total // 3  # 나뉘어질 구역의 가로 길이
+    return [[[row[idx:idx+small] for row in land[mid: mid+small]] for idx in range(0, total, small)] for mid in range(0, total, small)]
+ 
+def is_same(land):
+    '''
+    입력으로 주어진 구역이 같은 작물로만 이루어졌는지 판단하는 함수입니다.
+    구역의 첫 번째 원소를 기준으로 삼아 구역을 돌면서 다른 값이 존재하는 경우 False를 반환합니다.
+    '''
+    criteria = land[0][0]
+    for row in range(len(land)):
+        for col in range(len(land)):
+            if land[row][col] != criteria:
+                return False
+    return True
+ 
+if __name__ == '__main__':
+    '''
+    상추, 오이, 토마토가 심어진 구역의 개수를 체크하는 전역변수 count_list를 설정하였습니다.
+    '''
+    count_list = [0, 0, 0] 
+    wk5_4()
